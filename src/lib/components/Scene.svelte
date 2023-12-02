b525e0dd6ec37be7aeb7be05c2efd12fe3c63c61
Aaron Blenkush <aaronblenkush@gmail.com>
2023-12-01 23:09:49 -0900

    pyramid

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,60 +1,127 @@
 <script>
-  import { T, useFrame } from '@threlte/core'
+import { T, useTask } from '@threlte/core';
+import {
+    Grid,
+    MeshLineGeometry,
+    MeshLineMaterial,
+    OrbitControls,
+} from '@threlte/extras';
+import { CatmullRomCurve3, Vector3 } from 'three';
+import { onMount } from 'svelte';
 
-  let rotation = 0
-  useFrame(() => {
-    rotation += 0.001
-  })
+function makePoints(points,coords=[]) {
+    let vertices = [];
+    while(coords.length > 0) {
+        vertices.push(Reflect.construct(Vector3,coords.slice(0,3)));
+        coords = coords.slice(3);
+    }
+    const curve = new CatmullRomCurve3(vertices);
+    return curve.getPoints(points);
+}
+
+let lines = [
+    [60,-2, 0,-2,-3, 0, 0,-2, 0, 2,], // CL
+    [2, -2, 0, 2,-2, 0,-2,],          // L
+    [2,  0, 4, 0,-2, 0, 2,],          // SW
+    [2, -2, 0,-2, 0, 4, 0,],          // NW
+    [60, 2, 0,-2, 0, 0,-3,-2, 0,-2,], // CT
+    [2, -2, 0,-2, 2, 0,-2,],          // T
+    [2,  2, 0, 2,-2, 0,-2,],          // \
+    [2, -2, 0, 2, 2, 0, 2,],          // B
+    [60, 2, 0, 2, 0, 0, 3,-2, 0, 2,], // CB
+    [2,  2, 0,-2, 2, 0, 2,],          // R
+    [2,  0, 4, 0, 2, 0,-2,],          // NE
+    [2,  2, 0, 2, 0, 4, 0,],          // SE
+    [60, 2, 0,-2, 3, 0, 0, 2, 0, 2,], // CR
+    [ 2,-2, 0, 2, 2, 0,-2,],          // /
+];
+
+let renderedLines = [];
+let dashRatio = [];
+let camera;
+let draw = true;
+let ctl;
+let rotate = 15;
+
+function advance() {
+    if (renderedLines.length == lines.length) {
+        draw = false;
+        if (ctl) clearInterval(ctl);
+    }
+    let i = renderedLines.length+1;
+    renderedLines = lines.slice(0,i);
+    dashRatio[i-1] = 1;
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+onMount(async () => {
+    await delay(1000);
+    ctl = setInterval(advance,500);
+    return () => {
+        clearInterval(ctl);
+    }
+});
+
+useTask((delta) => {
+    if (draw) {
+        let i = renderedLines.length - 1;
+        for (let i=0;i<renderedLines.length;i++) {
+            if (dashRatio[i] > 0) {
+                dashRatio[i] = Math.max(0,dashRatio[i] - delta * 3);
+            }
+        }
+    } else {
+        if (rotate > 0) {
+            rotate = Math.max(0,rotate - delta * 10);
+            camera.position.set(0,15,rotate);
+        }
+    }
+});
+
 </script>
 
-<T.Group rotation.y={rotation}>
-  <T.PerspectiveCamera
-    makeDefault
-    position={[-10, 10, 10]}
-    fov={15}
-    on:create={({ ref }) => {
-      ref.lookAt(0, 1, 0)
-    }}
-  />
-</T.Group>
-
-<!-- Floor -->
-<T.Mesh rotation.x={(90 * Math.PI) / 180}>
-  <T.CircleGeometry args={[3, 16]} />
-  <T.MeshBasicMaterial
-    color="#666666"
-    wireframe
-  />
+{#each renderedLines as [sections,...line],i}
+{@const points = makePoints(sections,line)}
+<T.Mesh position.y={0}>
+    <MeshLineGeometry {points} />
+    <MeshLineMaterial
+        color="#0000ff"
+        width={0.1}
+        dashArray={1}
+        dashRatio={dashRatio[i]}
+        dashOffset=0
+        depthTest={false}
+        transparent
+    />
 </T.Mesh>
+{/each}
+
+<T.PerspectiveCamera
+    makeDefault
+    fov={30}
+    on:create={({ ref }) => {
+        camera = ref;
+        ref.position.set(0, 15, 15);
+    }}
+>
+    <OrbitControls
+        autoRotate={false}
+        autoRotateSpeed={0}
+        enableDamping
+        target.y={2}
+    />
+</T.PerspectiveCamera>
 
 <T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
+    position={[0, 10, 10]}
+    castShadow
 />
-<T.AmbientLight intensity={0.2} />
 
-<T.Mesh
-  position.y={1.2}
-  position.z={-0.75}
->
-  <T.BoxGeometry />
-  <T.MeshStandardMaterial color="#0059BA" />
-</T.Mesh>
-
-<T.Mesh
-  position={[1.2, 1.5, 0.75]}
-  rotation.x={5}
-  rotation.y={71}
->
-  <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-  <T.MeshStandardMaterial color="#F85122" />
-</T.Mesh>
-
-<T.Mesh
-  position={[-1.4, 1.5, 0.75]}
-  rotation={[-5, 128, 10]}
->
-  <T.IcosahedronGeometry />
-  <T.MeshStandardMaterial color="#F8EBCE" />
-</T.Mesh>
+<Grid
+    gridSize={[20, 20]}
+    cellColor={"#46536b"}
+    sectionThickness={0}
+/>
